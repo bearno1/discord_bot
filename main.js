e4c5d962af33dcd42afac2777726f343e5f8bad3
bearno1 <kaninsk139@gmail.com>
2020-06-23 10:30:56 +0700

    Update main.js

--- main.js
+++ main.js
@@ -12,102 +12,96 @@ client.once('ready',() => {
 client.login(process.env.token);
 
 client.on('message', msg => { 
-    let spl = msg.content.substring(prefix.length).split('\n');
-    var spl_size = spl.length;
-    var splrr;
+    let arg = msg.content.substring(prefix.length).split('\n');
+    if(!msg.content.startsWith(prefix))return;
+    if(arg[2]=='error'){
+      msg.reply('Notename is incorrect.');
+      return;
+    }
 
-    for(splrr=0;splrr<spl_size;splrr++){
-      let arg = spl[splrr].content.substring(prefix.length).split(' ');
-      if(!spl[splrr].content.startsWith(prefix))return;
-      if(arg[2]=='error'){
-        msg.reply('Notename is incorrect.');
-        return;
-      }
-
-      var arg_size = arg.length;
-      var note_size = note.length;
-      var isnote = 'false';
-      var i,j,rr=1;
-      var note_print;
-      var isprint = 'false';
-      var isnoteprint = 'false';
-      switch(arg[1]){
-          case 'test':
-            msg.channel.send('Hi! I am Bot.');
-            break;
-          case '!newnote':
-            isprint = 'true';
-            isnote = 'true';
-            note_print = "new note "+arg[2];
-            note.push(arg[2]);
-            collect.push(new Array());
-            break;
-          case '!printnote':
-            isprint = 'true';
-            isnoteprint = 'true';
-            note_print = arg[2]+':'+'\n';
-            for(i=0;i<note_size;i++){
-              if(note[i]==arg[2]){
-                isnote = 'true';
-                var collect_size = collect[i].length;
-                for(j=0;j<collect_size;j++){
-                  note_print += (j+1);
-                  note_print += '.';
-                  note_print += collect[i][j];
-                  note_print += '\n';
-                }
+    var arg_size = arg.length;
+    var note_size = note.length;
+    var isnote = 'false';
+    var i,j,rr=1;
+    var note_print;
+    var isprint = 'false';
+    var isnoteprint = 'false';
+    switch(arg[1]){
+        case 'test':
+          msg.channel.send('Hi! I am Bot.');
+          break;
+        case '!newnote':
+          isprint = 'true';
+          isnote = 'true';
+          note_print = "new note "+arg[2];
+          note.push(arg[2]);
+          collect.push(new Array());
+          break;
+        case '!printnote':
+          isprint = 'true';
+          isnoteprint = 'true';
+          note_print = arg[2]+':'+'\n';
+          for(i=0;i<note_size;i++){
+            if(note[i]==arg[2]){
+              isnote = 'true';
+              var collect_size = collect[i].length;
+              for(j=0;j<collect_size;j++){
+                note_print += (j+1);
+                note_print += '.';
+                note_print += collect[i][j];
+                note_print += '\n';
               }
             }
-            break;
-          case '!addnote':
-            isprint = 'true';
-            note_print = 'add '+arg[2]+':';
-            for(i=0;i<note_size;i++){
-              if(note[i]==arg[2]){
-                isnote = 'true';
-                var collect_size = collect[i].length;
-                for(j=3;j<arg_size;j++){
-                  if(j!=3)note_print += ' ';
-                  note_print += arg[j];
-                  if(j==3)collect[i].push(arg[j]);
-                  else collect[i][collect_size]+=' '+arg[j];
-                }
+          }
+          break;
+        case '!addnote':
+          isprint = 'true';
+          note_print = 'add '+arg[2]+':';
+          for(i=0;i<note_size;i++){
+            if(note[i]==arg[2]){
+              isnote = 'true';
+              var collect_size = collect[i].length;
+              for(j=3;j<arg_size;j++){
+                if(j!=3)note_print += ' ';
+                note_print += arg[j];
+                if(j==3)collect[i].push(arg[j]);
+                else collect[i][collect_size]+=' '+arg[j];
               }
             }
-            break;
-          case '!removenote':
-            isprint = 'true';
-            note_print = 'remove '+arg[2];
-            for(i=0;i<note_size;i++){
-              if(note[i]==arg[2]){
-                note[i]='error';
-                isnote = 'true';
-              }
+          }
+          break;
+        case '!removenote':
+          isprint = 'true';
+          note_print = 'remove '+arg[2];
+          for(i=0;i<note_size;i++){
+            if(note[i]==arg[2]){
+              note[i]='error';
+              isnote = 'true';
             }
-            break;
-          case '!list':
-            isnote = 'true';
-            isprint = 'true';
-            note_print = 'list:\n';
-            for(i=0;i<note_size;i++){
-              if(note[i]!='error'){
-                note_print += rr;
-                rr++;
-                note_print += '.'+note[i]+'\n';
-              }
+          }
+          break;
+        case '!list':
+          isnote = 'true';
+          isprint = 'true';
+          note_print = 'list:\n';
+          for(i=0;i<note_size;i++){
+            if(note[i]!='error'){
+              note_print += rr;
+              rr++;
+              note_print += '.'+note[i]+'\n';
             }
-            break;
-          default:
-            msg.channel.send('I can\'t understand you');
-            break;
-      }
-      if(isnoteprint == 'true'){
-        if(isnote == 'true')msg.channel.send(note_print);
-        else msg.reply('note not found');
-      }
-      else if(isprint == 'true'){
-        if(isnote == 'true')msg.reply(note_print);
-        else msg.reply('note not found');
-      }
+          }
+          break;
+        default:
+          msg.channel.send('I can\'t understand you');
+          break;
+    }
+    if(isnoteprint == 'true'){
+      if(isnote == 'true')msg.channel.send(note_print);
+      else msg.reply('note not found');
+    }
+    else if(isprint == 'true'){
+      if(isnote == 'true')msg.reply(note_print);
+      else msg.reply('note not found');
     }
 });
