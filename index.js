0c9f17fa32a84687cc2d1bc3754fddc4f0e8e91c
bearno1 <kaninsk139@gmail.com>
2021-02-02 00:08:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const client = new Discord.Client();
 const prefix = '!';
 var answer=" ";
 var room;
+var isroom = false;
 
 client.login('NzI0NDc1MDgyOTU2NzM0NTA0.XvAt_w._P8PwIfMJnqcQj64NHF0_Ih0foY');
 
@@ -13,11 +14,17 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if(msg.member.displayName=="Blueberry")return;
-  if(room!=msg.channel) {
-    room.send(msg.content);
+  if(isroom) {
+    if(msg.channel!=room) {
+      room.send(msg.content);
+    }
+    else {
+      room=msg.channel;
+    }
   }
   else {
     room=msg.channel;
+    isroom = true;
   }
   if(msg.content[0]!=prefix)return;
   let mes = msg.content.substring(prefix.length).split(" ");
