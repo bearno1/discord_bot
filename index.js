95de5a26834aed54710df310ef21c52f207d759f
bearno1 <kaninsk139@gmail.com>
2021-02-01 17:15:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,23 +17,8 @@ client.on('message', msg => {
   if(msg.member.voice.channel) {
     msg.member.voice.channel.join();
   }
-  if(msg.member.displayName=="Groovy") {
-    if(playlist<CountMyplaylist){
-      msg.channel.send("-play " + Myplaylist[playlist]);
-      playlist++;
-    }
-    else {
-      msg.channel.send("-shuffle");
-    }
-  }
-  //msg.channel.send(msg.member.displayName);
+  msg.channel.send("Hi "+msg.member.displayName);
   if(msg.content[0]!=prefix)return;
   let mes = msg.content.substring(prefix.length).split(" ");
-  msg.channel.send(mes[0]);
-  switch(mes[0]) {
-    case 'play': 
-      msg.channel.send("ok");
-      break;
-  }
   return;
 });
