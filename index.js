406429871f96f112612eaf9350fa30acef8c5d88
bearno1 <kaninsk139@gmail.com>
2021-02-01 16:22:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if(msg[0]!=prefix)return;
+  if(msg.content[0]!=prefix)return;
   let mes = msg.content.substring(prefix.length).split(" ");
   msg.channel.send(mes[0]);
   switch(mes[0]) {
