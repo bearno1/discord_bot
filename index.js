2681996c9c1935c04e02e2016113d906c6a47868
bearno1 <kaninsk139@gmail.com>
2021-02-01 22:30:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const prefix = '!';
-const Greetinglist = ["Hi ","Hey ","สวัสดี "," Yo ","你好 ","Good Luck! "];
-const Countgreetinglist = Greetinglist.length;
-let Ngreetinglist = 0;
+var answer=" ";
+var room;
 
 client.login('NzI0NDc1MDgyOTU2NzM0NTA0.XvAt_w._P8PwIfMJnqcQj64NHF0_Ih0foY');
 
@@ -14,9 +13,12 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if(msg.member.displayName=="Blueberry")return;
-  msg.channel.send(Greetinglist[Ngreetinglist]+msg.member.displayName);
-  Ngreetinglist++;
-  if(Ngreetinglist>=Countgreetinglist)Ngreetinglist=0;
+  if(msg.member.displayName=="inwbearX") {
+    room.send(msg.content);
+  }
+  else {
+    room=msg.channel;
+  }
   if(msg.content[0]!=prefix)return;
   let mes = msg.content.substring(prefix.length).split(" ");
   return;
