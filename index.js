49d5ba3d470557ba6c80e3d624076dcea60b9d7b
inwbear <59408250+bearno1@users.noreply.github.com>
2022-10-02 00:36:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ function setPlayXOAI() {
   return;
 }
 
-client.login('NzI0NDc1MDgyOTU2NzM0NTA0.XvAt_w._P8PwIfMJnqcQj64NHF0_Ih0foY');
+client.login(TOKEN);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
